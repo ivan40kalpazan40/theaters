097ea0ae7f65a872725829c9628da6b0752bec19
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-18 16:14:36 +0300

    render register

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -3,7 +3,11 @@ const router = require('express').Router();
 const renderLogin = (req, res) => {
   res.render('user/login');
 };
+const renderRegister = (req, res) => {
+  res.render('user/register');
+};
 
 router.get('/login', renderLogin);
+router.get('/register', renderRegister);
 
 module.exports = router;
