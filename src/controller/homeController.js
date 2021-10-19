dc948ae726c1c6c565fecacd3172e6d8bfc0e707
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-19 14:24:24 +0300

    display plays on home for loggedOut users

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
+const playServices = require('../services/playServices');
 const { isGuest } = require('../middleware/authMiddleware');
 
-const renderHome = (req, res) => {
-  res.render('index', { user: req.user });
+const renderHome = async (req, res) => {
+  try {
+    const plays = await playServices.getAll();
+    let allPlays = plays
+      .map((x) => x._doc)
+      .sort((a, b) => b.usersLiked.length - a.usersLiked.length)
+      .slice(0, 3);
+    res.render('index', { user: req.user, plays: allPlays });
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
 };
 
 router.get('/', isGuest, renderHome);
